9b38a556d0d6fad9ef0c0f5bd9e2586b890016d0
Tanu-priya02 <121554388+Tanu-priya02@users.noreply.github.com>
2024-09-30 20:23:03 +0530

    Create consts.go

---
+++ consts.go
@@ -0,0 +1,10 @@
+const (
+	// TODO: choose a human-readable part for your hyperchain
+	HRP = "Token"
+	// TODO: choose a name for your hyperchain
+	Name = "Tanu"
+	// TODO: choose a token symbol
+	Symbol = "TP"
+)
+
+var ID ids.ID
